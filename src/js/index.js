4ef8e02056a2af34d664c6be582613a02f635d1b
Nikita Savanchuk <nikita.savanchuk@gmail.com>
2018-05-04 00:09:17 +0300

    + main page and half biography page

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@ import jQuery from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
 
-jQuery(function() {
-    jQuery('body').css('color', 'blue');
-});
+// jQuery(function() {
+//     jQuery('body').css('color', 'blue');
+// });
