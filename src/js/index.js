9aefcd2a263b6ea8efeb4b7eb9dcb264fee21a95
Nikita Savanchuk <nikita.savanchuk@gmail.com>
2018-05-10 00:00:30 +0300

    + модальная форма

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,32 @@
-import jQuery from 'jquery';
-import popper from 'popper.js';
-import bootstrap from 'bootstrap';
+import $ from 'jquery';
 
-// jQuery(function() {
-//     jQuery('body').css('color', 'blue');
-// });
+$('#exampleModalCenter h2').hide();
+$('#exampleModalCenter .btn-secondary').click(() => $('#exampleModalCenter').hide());
+
+$("#contact-us-form").submit(function (event) {
+
+    event.preventDefault();
+
+    let $form = $(this);
+
+    let $inputs = $('#exampleModalCenter').find(".btn-primary, #contact-us-form");
+
+    let serializedData = $form.serialize();
+
+    $inputs.hide();
+
+    $.ajax({
+        url: "/index.html",
+        type: "get",
+        data: serializedData,
+        success: function (response) {
+            $('#exampleModalCenter h2').show();
+            console.log(response);
+        },
+        error: function (response) {
+        }
+    });
+});
+
+
+$('.modal-footer').click(() => $("#contact-us-form").submit());
